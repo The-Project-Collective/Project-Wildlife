2c3779e1e23439148e540bc40c5f0e98a3bb07e1
mia <mia.bellclan@gmail.com>
2025-01-30 01:44:52 +0000

    Moved the hitbox code to Project Core.

--- common/src/main/java/com/collective/projectwildlife/entities/animals/mammals/fox/AmericanRedFoxEntity.java
+++ common/src/main/java/com/collective/projectwildlife/entities/animals/mammals/fox/AmericanRedFoxEntity.java
@@ -51,14 +51,6 @@ public class AmericanRedFoxEntity extends CoreAnimalEntity implements GeoAnimata
     public static final RawAnimation IDLE = RawAnimation.begin().thenLoop("animation.american_red.idle");
     public static final RawAnimation RUN = RawAnimation.begin().thenLoop("animation.american_red.run");
     
-    public float minHeight = 0.5f;
-    public float maxHeight = 1f;
-    public float minWidth = 0.4f;
-    public float maxWidth = 0.9f;
-
-    private int counter = 0;
-    
-    
     public AmericanRedFoxEntity(EntityType<? extends AnimalEntity> entityType, World world) {
         super(entityType, world, true, true, true, true, true, true, true, true);
     }
@@ -157,54 +149,6 @@ public class AmericanRedFoxEntity extends CoreAnimalEntity implements GeoAnimata
         this.setPack(List.of(this.getUuidAsString()));
         this.calculateDimensions();
         return super.initialize(world, difficulty, spawnReason, entityData);
-    }
-
-    @Override
-    protected EntityDimensions getBaseDimensions(EntityPose pose) {
-        return super.getBaseDimensions(pose).scaled(this.getDimensionScaleWidth(), this.getDimensionScaleHeight());
-    }
-
-    public float getDimensionScaleHeight() {
-        float step = (this.getMaxHeight() - this.minHeight) / ((this.getAdultDays() * 24000) + 1);
-        if (this.getAgeTicks() >= this.getAdultDays() * 24000) {
-            return this.minHeight + ((step) * this.getAdultDays() * 24000);
-        }
-        return minHeight + (step * this.getAgeTicks());
-    }
-
-    public float getDimensionScaleWidth() {
-        float step = (getMaxWidth() - this.minWidth) / ((this.getAdultDays() * 24000) + 1);
-        if (this.getAgeTicks() >= this.getAdultDays() * 24000) {
-            return this.minWidth + ((step) * this.getAdultDays() * 24000);
-        }
-        return minWidth + (step * this.getAgeTicks());
-    }
-
-    public float getMaxHeight() {
-        if (this.getGender() == 0) {
-            return maxHeight;
-        } else {
-            return maxHeight - 0.25f;
-        }
-    }
-
-    public float getMaxWidth() {
-        if (this.getGender() == 0) {
-            return maxWidth;
-        } else {
-            return maxWidth - 0.1f;
-        }
-    }
-
-    @Override
-    public void tickMovement() {
-        super.tickMovement();
-        if (counter < 20) {
-            counter++;
-        } else {
-            calculateDimensions();
-            counter = 0;
-        }
     }
 
     // === VARIANT CONTEXT =======================================================================================================================================================================
@@ -422,6 +366,36 @@ public class AmericanRedFoxEntity extends CoreAnimalEntity implements GeoAnimata
     @Override
     public float getFemaleMaxSize() {
         return 0.9f;
+    }
+
+    @Override
+    public float getMinHeight() {
+        return 0.5f;
+    }
+
+    @Override
+    public float getMinWidth() {
+        return 0.4f;
+    }
+
+    @Override
+    public float getMaxHeight() {
+        return 1f;
+    }
+
+    @Override
+    public float getMaxWidth() {
+        return 0.9f;
+    }
+
+    @Override
+    public float getHeightDifference() {
+        return 0.2f;
+    }
+
+    @Override
+    public float getWidthDifference() {
+        return 0.1f;
     }
 
     // --- Sounds ------------------------------------------------------------------------------------------
